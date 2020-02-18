57384c7376a9f157bcba438ede5808a0ab3342c5
muyuanqiang <muyuanqiang7@gmail.com>
2020-02-18 09:33:24 +0800

    add babel config and babel dependency

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+const presets = [
+    [
+        "@babel/env",
+        {
+            targets: {
+                edge: "17",
+                chrome: "64",
+                firefox: "60",
+                safari: "11.1"
+            }
+        }
+    ]
+]
+module.exports = {presets};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 const fn = () => 1; // 箭头函数, 返回值为1
 console.log(fn());
+
+let num = 3 ** 2;
+
+let foo = function (a, b, c,) { // ES7参数支持尾部逗号
+    console.log('a:', a);
+    console.log('b:', b);
+    console.log('c:', c)
+};
+foo(1, 3, 4);
+console.log(fn());
+console.log(num);
